68232f66b8567c3e4c563a5e33770963455b2059
Алексей <alexust11@yandex.ru>
2022-12-05 15:26:14 +0700

    Initial commit

---
+++ src/main/java/org/example/BinarySearch.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        List<Integer> numbers = new ArrayList( List.of(2, 3, 4, 14, 2, 80, 21, 3, 56, 79));// List.of нужно обернуть в new ArrayList для того чтобы можно было изменять массив, иначе List.of не изменяемый массив
+        for (int idxCurr = numbers.size() - 1; idxCurr >= 0; idxCurr--) {
+          int max=Integer.MIN_VALUE;// самое маленькое число
+            int idxMax=0;
+        for (int i = 0; i <= idxCurr; i++) {
+            final Integer element = numbers.get(i);
+            if (max < element) {
+                max= element;
+                idxMax=i;
+            }
+        }
+//        var idxCurr = numbers.size() - 1;
+        int tmp = numbers.get(idxCurr);
+        numbers.set(idxCurr, max);
+        numbers.set(idxMax, tmp);
+
+    }
+        System.out.println("numbers.toString() = " + numbers.toString());
+
+    }
+}
